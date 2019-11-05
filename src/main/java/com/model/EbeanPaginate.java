ae016ed9ae0c1ebb4cb632e4c1eb9c7a55c24edd
zhaofengdeng <zhao_fengdeng@khktsoft.com>
2019-11-05 08:53:52 +0800

    project init

---
+++ src/main/java/com/model/EbeanPaginate.java
@@ -0,0 +1,18 @@
+package com.model;
+
+public class EbeanPaginate {
+	/**
+	 * 当前页数
+	 */
+	public Integer curPage;
+	
+	/**
+	 * 每页显示的条数
+	 */
+	public Integer maxPerPage;
+	
+	/**
+	 * 总数据条数
+	 */
+	public Integer dataCount;
+}
